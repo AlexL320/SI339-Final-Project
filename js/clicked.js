38363555df9187ab2ca4444c9f76777c42c0aadd
AlexL320 <alexye@umich.edu>
2025-04-13 16:43:21 -0400

    edits

--- js/clicked.js
+++ js/clicked.js
@@ -1,11 +1,20 @@
 const sections = document.querySelectorAll("section");
 
 sections.forEach((section, index) => {
-  const image = section.getElementsByClassName("img")[0];
+  const image = section.getElementsByTagName("img")[0];
 
   if (image) { // Always good to check if image exists
     image.addEventListener("click", () => {
       console.log(`Image in section ${index + 1} was clicked`);
+      const stats = document.getElementById("stats");
+      if (stats.style.display == "block"){
+        stats.style.display = "none";
+        console.log("hide stats");
+      }
+      else if(stats.style.display == "none"){
+        stats.style.display = "block";
+        console.log("show stats");
+      }
     });
   }
 });
