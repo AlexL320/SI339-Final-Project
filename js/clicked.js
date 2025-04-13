39a1116146a5ceeb071868b613633af5c7f9e55d
Dennis Liu <dennisye@umich.edu>
2025-04-13 17:11:40 -0400

    did the click funciton

--- js/clicked.js
+++ js/clicked.js
@@ -6,12 +6,12 @@ sections.forEach((section, index) => {
   if (image) { // Always good to check if image exists
     image.addEventListener("click", () => {
       console.log(`Image in section ${index + 1} was clicked`);
-      const stats = document.getElementById("stats");
+      const stats = section.querySelector("#stats");
       if (stats.style.display == "block"){
         stats.style.display = "none";
         console.log("hide stats");
       }
-      else if(stats.style.display == "none"){
+      else {
         stats.style.display = "block";
         console.log("show stats");
       }
